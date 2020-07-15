8204bc10ce24c3a6e50db59342ca171f5c0f7491
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-07-15 19:12:55 +0200

    Trying to build a program with products. Now better.

--- src/ProductDatabaseInFile/Test.java
+++ src/ProductDatabaseInFile/Test.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) throws IOException {
         Product[] productsOne = new Product[3];
-        productsOne[0] = new Product("Marfel-GJ", "Lożitej", 169.99);
-        productsOne[1] = new Product("Small", "Elegom", 199.99);
-        productsOne[2] = new Product("Matrel", "Kenżyngton", 220.99);
+        productsOne[0] = new Product("Marfel-GJ;", "Lożitej;", 169.99);
+        productsOne[1] = new Product("Small;", "Elegom;", 199.99);
+        productsOne[2] = new Product("Matrel;", "Kenżyngton;", 220.99);
 
         String fileName = "testProductBoards.csv";
         try(
@@ -17,7 +17,6 @@ public class Test {
                 var writer = new BufferedWriter(fileWriter)
         ) {
             for (Product product : productsOne) {
-                writer.write(product + "\n");
                 writer.write(product.getName());
                 writer.write(" ");
                 writer.write(product.getProducer());
@@ -36,17 +35,18 @@ public class Test {
         var reader = new BufferedReader(fileReader)
 
         ){
+
             StringBuilder stringBuffer = new StringBuilder();
-            String eachLine =reader.readLine();
-            while (eachLine != null){
+            for (String eachLine = reader.readLine(); eachLine!= null; eachLine = reader.readLine()){
                 stringBuffer.append(eachLine);
                 stringBuffer.append("\n");
                 eachLine = reader.readLine();
                 String[] split = eachLine.split(";");
+                double a =Double.parseDouble(split[2]);
                 Product[] products = new Product[3];
-                products[0] = new Product(split[0], split[1], split[2]);
-                products[1] = new Product(split[0], split[1], split[2]);
-                products[2] = new Product(split[0], split[1], split[2]);
+                products[0] = new Product(split[0], split[1], a);
+                products[1] = new Product(split[0], split[1], a);
+                products[2] = new Product(split[0], split[1], a);
                 System.out.println(Arrays.toString(products));
             }
             System.out.println(stringBuffer.toString());
