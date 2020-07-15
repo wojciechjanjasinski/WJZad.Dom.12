f82215a7c3bcf581002a0c88a6741856d8d18cee
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-07-15 21:11:34 +0200

    Trying to build another program.

--- src/CompanyStatistics/Test.java
+++ src/CompanyStatistics/Test.java
@@ -1,33 +1,36 @@
 package CompanyStatistics;
 
+import ProductDatabaseInFile.Product;
+
 import java.io.*;
 import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         String fileName = "InputData.txt";
-        String[] employeesString = new String[1];
-        System.out.println(Arrays.toString(employeesString));
-        Employee[] employees = Arrays.copyOf(employeesString, employeesString.length, Employee[].class);
-        System.out.println(Arrays.toString(employees));
+
         try (
                 var fileReader = new FileReader(fileName);
                 var reader = new BufferedReader(fileReader)
 
         ){
-            StringBuilder stringBuffer = new StringBuilder();
-            String eachLine =reader.readLine();
 
-            while (eachLine != null){
-                stringBuffer.append(eachLine);
-                stringBuffer.append("\n");
-                eachLine = reader.readLine();
-                for (String s : employeesString) {
-                    s = eachLine;
-                    System.out.println(s);
-                    Arrays.fill(employeesString, s);
-                }
+            for (String eachLine = reader.readLine(); eachLine!= null; eachLine = reader.readLine()){
+                String[] split = eachLine.split(";");
+                double b = Double.parseDouble(split[3]);
+                double c = Double.parseDouble(split[5]);
+                Employee[] employees = new Employee[1];
+                employees[0] = new Employee(split[1], split[2], b, split[4], c);
+                System.out.println(Arrays.toString(employees));
+
+                //String split 5
+                //double a =Double.parseDouble(split[2]);
+//                Product[] products = new Product[1];
+//                products[0] = new Product(split[0], split[1], a);
+//                System.out.println(Arrays.toString(products));
+
             }
+
         }
     }
 }
