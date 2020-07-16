3b4290177a1f7433d3483accfc32b27b29362fab
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-07-16 10:28:05 +0200

    Program number two works but didn,t have full functionality as wanted.

--- src/CompanyStatistics/Test.java
+++ src/CompanyStatistics/Test.java
@@ -1,36 +1,32 @@
 package CompanyStatistics;
 
-import ProductDatabaseInFile.Product;
-
 import java.io.*;
 import java.util.Arrays;
+import java.util.stream.DoubleStream;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         String fileName = "InputData.txt";
-
+        Employee[] employees = new Employee[1];
         try (
                 var fileReader = new FileReader(fileName);
                 var reader = new BufferedReader(fileReader)
 
-        ){
+        ) {
 
-            for (String eachLine = reader.readLine(); eachLine!= null; eachLine = reader.readLine()){
+            for (String eachLine = reader.readLine(); eachLine != null; eachLine = reader.readLine()) {
                 String[] split = eachLine.split(";");
-                double b = Double.parseDouble(split[3]);
-                double c = Double.parseDouble(split[5]);
-                Employee[] employees = new Employee[1];
-                employees[0] = new Employee(split[1], split[2], b, split[4], c);
+                double b = Double.parseDouble(split[2]);
+                double c = Double.parseDouble(split[4]);
+                employees[0] = new Employee(split[0], split[1], b, split[3], c);
                 System.out.println(Arrays.toString(employees));
-
-                //String split 5
-                //double a =Double.parseDouble(split[2]);
-//                Product[] products = new Product[1];
-//                products[0] = new Product(split[0], split[1], a);
-//                System.out.println(Arrays.toString(products));
-
+                double something = 0;
+                double sum = DoubleStream.of(c).sum();
+                something = something + sum;
+                System.out.println(something);
             }
-
         }
+
+
     }
 }
